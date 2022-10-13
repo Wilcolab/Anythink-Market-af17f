9d192db743fdc2f35c27bc26cded51c9552b9e09
Ofer Lis <oferlis1@gmail.com>
2022-10-13 14:59:49 +0300

    Update items_resource.py

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -46,8 +46,8 @@ async def list_items(
         ItemForResponse.from_orm(item) for item in items
     ]
 
-    for item in items_for_response:
-        item = handle_missing_image(item) 
+    # for item in items_for_response:
+    #     item = handle_missing_image(item) 
     
     return ListOfItemsInResponse(
         items=items_for_response,
@@ -91,7 +91,7 @@ async def retrieve_item_by_slug(
 ) -> ItemInResponse:
     item_ret = ItemInResponse(item=ItemForResponse.from_orm(item))
     
-    item_ret.item = handle_missing_image(item_ret.item)
+    #item_ret.item = handle_missing_image(item_ret.item)
     return item_ret
 
 
