02419d3a618adae6ef3a1a9cfe11e5226a5e1995
Ofer Lis <oferlis1@gmail.com>
2023-03-10 17:52:34 +0200

    fixed

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+// import placeholderImage from '../../public/placeholder.png'
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -19,6 +20,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  const placeholderImage = '/placeholder.png';
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -37,7 +39,8 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image || placeholderImage}
+        onerror="this.onerror=null; this.src='placeholder.png'"
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
